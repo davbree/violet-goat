143c98a9d4a38f12133d6254f9580b656a531d17
Stackbit Code Editor <projects@stackbit.com>
2021-07-25 09:35:41 +0000

    CtaSection.js: changed

--- src/components/CtaSection.js
+++ src/components/CtaSection.js
@@ -20,7 +20,7 @@ export default class CtaSection extends React.Component {
                     )}
                     <div className={classNames('section__content', {'cell': _.get(section, 'image', null)})}>
                       {_.get(section, 'title', null) && (
-                      <h2 className="section__title">{_.get(section, 'title', null)}</h2>
+                      <h6 className="section__title">{_.get(section, 'title', null)}</h6>
                       )}
                       {_.get(section, 'content', null) && (
                       <div className="section__body text-block">
